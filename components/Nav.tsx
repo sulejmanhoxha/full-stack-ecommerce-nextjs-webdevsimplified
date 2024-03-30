465e5b413cfb8fe7e37f0eaf2b4afc4c07bfe35f
sulejmanhoxha <sulejman.hoxha2001@hotmail.com>
2024-03-30 02:29:23 +0100

    feat: add nav

---
+++ components/Nav.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ComponentProps } from "react";
+
+import { cn } from "@/lib/utils";
+
+export function Nav({ children }: { children: React.ReactNode }) {
+  return (
+    <nav className="flex justify-center bg-primary px-4 text-primary-foreground">
+      {children}
+    </nav>
+  );
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+  const pathname = usePathname();
+  return (
+    <Link
+      {...props}
+      className={cn(
+        "p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground",
+        pathname === props.href && "bg-background text-foreground",
+      )}
+    />
+  );
+}
